fc1ba4c9169af3d9c438d37610d40dc2095d51a5
Leon Di Stefano <distefano.l@wehi.edu.au>
2015-09-22 15:22:23 +1000

    Add prerequisites.

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@
 
 from distutils.core import setup
 
-setup(name='sv_tools',
-      version='0.1',
-      description='Assorted tools for working with structural variants',
-      author='Leon Di Stefano',
-      author_email='distefano.l@wehi.edu.au',
+setup(name = 'sv_tools',
+      version = '0.1',
+      description = 'Assorted tools for working with structural variants',
+      author = 'Leon Di Stefano',
+      author_email = 'distefano.l@wehi.edu.au',
       # url='',
-      packages=['sv_tools'],
+      packages = ['sv_tools'],
+      install_requires = ['matplotlib', 'networkx',
+                          'pandas', 'numpy', 'itertools', 're', 'scipy']
      )
